0e12efb54d5b7f5ff445fa5b0b209e1f1857ea3b
paullaster <paullasterokoth98@gmail.com>
2024-06-20 01:56:28 +0300

    update art api

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,8 +3,8 @@ import express from "express";
 const cartRoutes = express.Router();
 
 // ROUTES   --------------------------------
-cartRoutes.get('/:userId', function (req, res) {res.json({userId: req.params.userId})});
-cartRoutes.post('/:userId', function (req, res) {res.json({userId: req.params.userId})});
+cartRoutes.get('/', function (req, res) {res.json({userId: req.user.userId})});
+cartRoutes.post('/', function (req, res) {res.json({userId: req.user.userId})});
 cartRoutes.put('/:cartItemId', function (req, res) {res.json({cartItemId: req.params.cartItemId,})});
 cartRoutes.delete('/:cartItemId', function (req, res) {res.json({cartItemId: req.params.cartItemId,})});
 
