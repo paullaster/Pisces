ecfc44d51251b7a6dd86be6866e21d974377afc8
paullaster <paullaster>
2025-06-04 18:08:37 +0300

    update runtime script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,24 +1,30 @@
-module.exports = {
-    apps: [
-      {
-        name: 'noels-deliveries-api',
-        script: 'server.js',
-        env_file: "./.env.local",
-        watch: false,
-        ignore_watch:["node_modules"],
-        instances: 'max',
-        exec_mode: 'cluster',
-        max_memory_restart: '3G',
-        post_update:["npm install", "echo noels deliveries apis lauched"],
-        node_args: ["--max_old_space_size=500", "--env-file=.env"],
-        env_production: {
-          NODE_ENV: 'production',
-        },
-        env_development: {
-          NODE_ENV: 'development',
+const { join, dirname, resolve } = require("node:path");
+const { fileURLToPath } = require("node:url");
 
-        }
+const cwd = __dirname;
+
+module.exports = {
+  apps: [
+    {
+      name: 'Noels ecommerce',
+      cwd: resolve(cwd),
+      script: 'server.js',
+      env_file: `${join(cwd, '.env')}`,
+      watch: false,
+      ignore_watch: ["node_modules"],
+      instances: 'max',
+      exec_mode: 'cluster',
+      max_memory_restart: '3G',
+      post_update: ["npm install", "npm run migrate", "echo Noels ecommerce backend api service deployed"],
+      node_args: ["--max_old_space_size=500", `--env-file=${join(cwd, '.env')}`],
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+
       }
-    ]
-  };
-  
+    }
+  ]
+};
+
